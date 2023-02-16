a435da75ebed091b633259521382233ff4f9b498
MihirYJoshi <43663025+MihirYJoshi@users.noreply.github.com>
2023-02-16 15:34:27 -0800

    grade

--- grade.sh
+++ grade.sh
@@ -1,13 +1,57 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+rm error1
+
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if[-f student-submission/ListExamples.java]
+cd student-submission
+
+if [ -f ListExamples.java ];
 then
     echo "File Exists"
 else
-    echo "Files Does Not Exist"
+    echo "File Does Not Exist"
+    exit
+fi
+
+cd ..
+
+cp TestListExamples.java student-submission
+
+set +e
+
+cd student-submission
+
+echo 'javac ListExamples.java' > error1.txt
+
+if [ $(wc -w error1.txt | awk '{print $1}') -eq 0 ]
+then
+    echo "Compiled Successfully"
+else
+    echo "Compiler Failure"
+    exit
+fi
+
+
+cd ..
+cp -r lib student-submission
+cd student-submission
+
+
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > results.txt
+
+#######
+
+grep -i "FAILURES" results.txt > failCheck.txt
+
+if [ $(wc -w failCheck.txt | awk '{print $1}') -eq 0 ]
+then
+    echo "PASSED"
+else 
+    echo "FAILED"
     exit
 fi
