a3bed8872822dc7f8e4b195287ab0cbbcfa38fc5
Andy <andyzng9@gmail.com>
2023-03-16 13:39:39 -0700

    new change

--- grade.sh
+++ grade.sh
@@ -1,5 +1,5 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
+set +e
 
 rm -rf student-submission
 git clone $1 student-submission
@@ -11,7 +11,7 @@ if [ -f ListExamples.java ];
 then
     echo "File Exists"
 else
-    echo "File Does Not Exist"
+    echo "File Does Not Exist. Check if file has correct name or is under the right directory"
     exit
 fi
 
@@ -19,41 +19,47 @@ cd ..
 
 cp TestListExamples.java student-submission
 
-set +e
-
 cd student-submission
 
 javac ListExamples.java 2> error1.txt
-echo what is in error1?:
 
-echo END OF error1
 if [ $(wc -w error1.txt | awk '{print $1}') -eq 0 ]
 then
     echo "Compiled Successfully"
 else
+    cat error1.txt
     echo "Compiler Failure"
     exit
 fi
-rm error1.txt
 
 cd ..
 cp -r lib student-submission
 cd student-submission
 
-
-
-#javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-#java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > results.txt
- java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > results.txt 
-#######
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > results.txt 
+
+cat results.txt
+
+num_tests=$(grep -oE 'Tests run: ([0-9]+)' results.txt | grep -oE '[0-9]+')
+
+num_fails=$(grep -oE 'Failures: ([0-9]+)' results.txt | grep -oE '[0-9]+')
 
 grep -i "FAILURES" results.txt > failCheck.txt
 
+grep " static List<String> filter(List<String> list, StringChecker sc)" ListExamples.java > method.txt
+
+if [ $(wc -w method.txt | awk '{print $1}') -eq 0 ]
+then echo "Incorrect filter method signature!"
+fi
+
+
 if [ $(wc -w failCheck.txt | awk '{print $1}') -eq 0 ]
 then
-    echo "PASSED"
+    echo "TEST PASSED"
+    echo "Score: 100%"
 else 
-    echo "FAILED"
+    echo "TEST FAILED"
+    echo "Score:" $((100 * ($num_tests-$num_fails) / $num_tests))"%"
     exit
 fi
